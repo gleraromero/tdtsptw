dd200602efd5438eefa2d679ecd82b879c26bfb0
Gonzalo Lera Romero <gleraromero@gmail.com>
2020-05-21 15:25:02 -0300

    Debug in prod

--- code/src/relaxation_solver.cpp
+++ code/src/relaxation_solver.cpp
@@ -88,9 +88,9 @@ GraphPath reconstruct_path(const VRPInstance& vrp, const NGLInfo& ngl_info,
 					path.push_back(u);
 					S = s;
 					if (ngl_info.L[r] == u) r--;
-					v = u;
 					time = time_u;
-					cost = cost + (time - time_u) - penalties[v];
+					cost = cost + (time - time_u) + penalties[v];
+					v = u;
 					clog << path << " at time " << time << " with cost " << cost << " because " << v << " has a penalty " << penalties[v] << endl;
 					break;
 				}
