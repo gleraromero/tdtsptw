239d121d48b9f2938e35e8b65dd0fe1e41de59dc
Gonzalo Lera Romero <gleraromero@gmail.com>
2020-05-21 14:23:43 -0300

    Debug in prod

--- code/src/relaxation_solver.cpp
+++ code/src/relaxation_solver.cpp
@@ -63,7 +63,7 @@ GraphPath reconstruct_path(const VRPInstance& vrp, const NGLInfo& ngl_info,
 				// Check if extension of l into v gives the last label in the path.
 				auto l_v = l.Extend(vrp, ngl_info, k, u, v, penalties[v]);
 				double cost_lv = l_v.CostAt(time);
-				if (epsilon_smaller(cost_lv, cost))
+				if (epsilon_smaller(cost_lv, cost) || v == 5)
 				{
 					clog.precision(8);
 					clog << "k: " << k << endl;
